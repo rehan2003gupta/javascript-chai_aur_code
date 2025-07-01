70b70f097e8e21611fa1484ffc42dbf035068d7e
rehan2003gupta <codebuddy051@gmail.com>
2025-07-01 08:30:16 +0530

    changes commited

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("Hello, World!");
 console.log("This is a test file.");
 console.log("Let's see if this works.");
+console.log("This is another line of code.");
+console.log("Adding more functionality to the script.");
