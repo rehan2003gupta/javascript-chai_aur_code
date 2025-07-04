3b22a040f37a52fe9e2d5b86b5d4ed8299b9253c
rehan2003gupta <codebuddy051@gmail.com>
2025-07-04 09:00:49 +0530

    advanced features

--- 01-basics/project3.js
+++ 01-basics/project3.js
@@ -1,31 +1,43 @@
 const InputNum = document.getElementById("guess");
 const Button = document.querySelector(".submit");
 const result = document.querySelector(".result");
-
-const randomNumber = Math.floor(Math.random() *10)+1;
-
+let times = 0;
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+const remain = document.querySelector(".remain");
 Button.addEventListener('click', function (event) {
   event.preventDefault();
-  if(InputNum.value == randomNumber) {
-    result.textContent = "Congratulations! You guessed the number!";
-  } else {
-    result.textContent = "Sorry, that's not correct. Try again!";
+  times++;
+  if (times < 10) {
+    if (InputNum.value == randomNumber) {
+      result.textContent = "Congratulations! You guessed the number!";
+      times = 0; // Reset the counter after a correct guess
+      resetGame(); // Reset the game for a new round
+    } else {
+      result.textContent = "Sorry, that's not correct. Try again!";
+      remain.textContent = ` remaining chances ${10-times}`;
+    }
+  }
+  else {
+    result.textContent = "Game Over! You've used all your chances.";
+    remain.textContent = '';
   }
 });
 function resetGame() {
   InputNum.value = '';
   result.textContent = '';
-  randomNumber = Math.floor(Math.random() *10)+1;
+  remain.textContent = '';
+  randomNumber = Math.floor(Math.random() * 10) + 1;
 }
 Button.addEventListener('dblclick', resetGame);
-function updatecursor(){
-    if(InputNum.value.trim() === ""){
-        Button.style.cursor = "not-allowed";
-        Button.disabled = true;
-    }
-    else{
-        Button.style.cursor = "pointer";
-        Button.disabled = false;
-    }
+
+function updatecursor() {
+  if (InputNum.value.trim() === "") {
+    Button.style.cursor = "not-allowed";
+    Button.disabled = true;
+  }
+  else {
+    Button.style.cursor = "pointer";
+    Button.disabled = false;
+  }
 }
 Button.addEventListener('mouseover', updatecursor);
